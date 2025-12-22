be25a4266f84cdf740f5f326c80e4501b40502f9
Gleb <glebgaevoy1@gmail.com>
2025-12-22 17:35:17 +0300

    Title screen looks good, but it could use some music.

--- scenes/titlescreen.py
+++ scenes/titlescreen.py
@@ -6,8 +6,16 @@ import config
 class title_screen:
     def __init__(self, screen):
         self.screen = screen
-        self.font = pygame.font.Font(None, 20)
-        self.title_text = self.font.render("Bhudda Simulator 2025", True, (255, 255, 255))
+        self.font = pygame.font.Font(None, 50)
+        self.title_text = self.font.render("Bhudda Simulator 2025", True, (0, 0, 0))
+        self.instruction_caption = self.font.render("PRESS SPACE TO START // PRESS Q TO QUIT", True, (255, 0, 0))
+
+        self.instruction_caption_rect = self.instruction_caption.get_rect()
+        self.instruction_caption_rect.centerx = config.SCREEN_WIDTH // 2
+        self.instruction_caption_rect.bottom = config.SCREEN_HEIGHT - 30
+        self.instruction_caption_center = (self.screen.get_width() / 2, self.screen.get_height() / 2)
+        self.highlight_rect = self.instruction_caption_rect.inflate(20, 20)
+
 
     def handle_event(self):
         for event in pygame.event.get():
@@ -29,5 +37,7 @@ class title_screen:
 
     def draw(self):
         self.screen.fill((0, 0, 0))
-        text_rect = self.title_text.get_rect(center=(self.screen.get_width() / 2, self.screen.get_height() / 2))
-        self.screen.blit(self.title_text, text_rect)
+        self.screen.blit(pygame.image.load("backgrounds/titlescreen_bg_1.jpg"), (0, 0))
+        self.screen.blit(self.title_text, (10, 10))
+        pygame.draw.rect(self.screen,(255, 255, 0), self.highlight_rect)
+        self.screen.blit(self.instruction_caption, self.instruction_caption_rect)
